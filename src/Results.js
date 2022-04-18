feca6fdecdae9d0e8e7098db530b4cda3bef4ebc
Rachel Collins <spiritedndiligent@gmail.com>
2022-04-17 23:19:48 -0400

    adding Results compo

---
+++ src/Results.js
@@ -0,0 +1,35 @@
+import Pet from "./Pet";
+
+ export default function Results ({ pets }){
+     return (
+         <div className="search" >
+             {
+             !pets.length ? <h2> No Pets Found</h2> : 
+             (
+                pets.map(pet => (
+                <Pet 
+                name={pet.name} 
+                animal={pet.animal}
+                breed={pet.breed}
+                key={pet.id}
+                images={pet.images}
+                location={`${pet.city}, ${pet.state}`}
+                />
+                    )
+                )
+            )
+             
+             }
+            {/* {pets.map(pet => (
+            <Pet name={pet.name} 
+            animal={pet.animal}
+            breed={pet.breed}
+            key={pet.id}
+            />
+            )
+            )} */}
+         </div>
+      
+     )
+    
+ }
