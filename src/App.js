a7a2e0cdd0e9a4b7d298cd8f2bf95b786e3c3da3
Rachel Collins <spiritedndiligent@gmail.com>
2022-04-20 20:42:29 -0400

    components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import { StrictMode } from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SearchParams from './SearchParams';
+import Details from './Details';
 //babel will import jsx 
 //sublevel components
 //top level component
     //subcomponents within it
-import ReactDOM from "react-dom";
 
-const App = () => (
+const App = () => {
+
+    return (
         <div>
-            <h1>Adopt Me</h1>
-            <SearchParams />
-        </div>
+        {/* <h1>Adopt Me</h1> */}
+        <Router>
+             <Route path= "/details/:id">
+                 <Details />
+             </Route>
+             <Route path="/">
+                 <SearchParams />
+            </Route>
+        </Router>
+       
+    </div>
     )
-//create and return a reat element (first arf)
-ReactDOM.render(<App />, document.getElementById("root"));
+}
+    
+
+       
+    
+//create and return a react element (first arg) strictmode for stronger error messages
+ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById("root"));
